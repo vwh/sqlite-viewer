fa6cc3f033a696694d1c5bfb62c0020148a1eb71
vwh <vwhe@proton.me>
2024-06-24 14:01:41 +0300

    make it ready for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/sqlite-viewer/",
   plugins: [react()],
   resolve: {
     alias: {
