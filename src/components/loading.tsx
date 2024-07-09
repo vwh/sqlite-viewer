768a85e6a6ce20ab351902b4b457fb22a6afb950
vwh <vwhe@proton.me>
2024-07-09 20:49:01 +0300

    making loadder better

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -7,7 +7,7 @@ export default function Loading() {
   return (
     <>
       {isLoading && (
-        <div className="flex items-center justify-center mb-4">
+        <div className="flex items-center justify-center mb-2 font-semibold p-4 border rounded">
           <Loader className="w-5 h-5 animate-spin" />
           <span className="ml-2">Reading SQLite file</span>
         </div>
