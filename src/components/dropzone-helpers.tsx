c9a226ccca3b4ccc3338826a7b6ff70519bf34eb
vwh <vwhe@proton.me>
2024-07-24 20:42:41 +0300

    added sonner for errors

--- src/components/dropzone-helpers.tsx
+++ src/components/dropzone-helpers.tsx
@@ -1,7 +1,7 @@
 import type { FileError } from "react-dropzone";
 import { bytesToValue } from "@/lib/file";
 
-import { FileWarning, FileCheck } from "lucide-react";
+import { toast } from "sonner";
 
 export function FileData({ file }: { file: File }) {
   return (
@@ -16,22 +16,12 @@ export function FileData({ file }: { file: File }) {
 }
 
 export function FileStats({ errors }: { errors?: FileError[] }) {
-  return errors ? (
+  errors && (
     <>
-      {errors.map((error) => (
-        <div
-          className="flex items-center justify-center text-sm text-red-600 gap-2 rounded border border-red-600 p-4 mb-2"
-          key={error.code}
-        >
-          <FileWarning />
-          {error.message}
-        </div>
-      ))}
+      {errors.map((error) =>
+        toast(error.message, { position: "bottom-right" }),
+      )}
     </>
-  ) : (
-    <li className="flex w-full items-center justify-center text-sm text-green-600">
-      <FileCheck className="mr-2" />
-      Uploaded successfully
-    </li>
   );
+  return <></>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
+import { Toaster } from "@/components/ui/sonner";
 
 import "./index.css";
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <main className="container mx-auto p-4 h-screen flex flex-col gap-3">
       <App />
     </main>
+    <Toaster />
   </React.StrictMode>,
 );
