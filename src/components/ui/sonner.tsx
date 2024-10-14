0832b99b063e41550293f9d138ee60c494240984
vwh <vwhe@proton.me>
2024-10-15 01:58:08 +0300

    chore: update toast colors

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -9,7 +9,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-primary group-[.toaster]:text-background group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-gray-100 p-2 dark:group-[.toaster]:bg-gray-700 text-lg p-4 group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
