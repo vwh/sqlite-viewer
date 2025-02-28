3a504bc956d34ad83ac7752434bf1ae81a7e6201
vwh <vwhe@proton.me>
2025-02-28 23:34:47 +0300

    feat: added the main SQLite logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,70 @@
 import { Button } from "@/components/ui/button";
+import Sqlite from "./lib/sqlite";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [sqlite, setSqlite] = useState<Sqlite | null>(null);
+
+  useEffect(() => {
+    // Initialize the sqlite instance asynchronously.
+    async function initDb() {
+      const instance = await Sqlite.create();
+      setSqlite(instance);
+    }
+    initDb();
+  }, []);
+
+  function handleFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      const arrayBuffer = e.target?.result as ArrayBuffer;
+      const uint8Array = new Uint8Array(arrayBuffer);
+      const instance = await Sqlite.open(uint8Array);
+      setSqlite(instance);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
+  function getTableNames() {
+    if (!sqlite) return;
+    console.log(sqlite.tables);
+  }
+
+  function getDatabaseSchema() {
+    if (!sqlite) return;
+    console.log(sqlite.schema);
+  }
+
+  function executeSql() {
+    if (!sqlite) return;
+    console.log(sqlite.exec(query));
+  }
+
+  // If the sqlite instance is not initialized, return a loading state.
+  if (!sqlite) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-svh">
-        <Button>Click me</Button>
-      </div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <input
+        type="text"
+        placeholder="Enter SQL"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <Button onClick={executeSql}>Execute SQL</Button>
+
+      <Button onClick={getTableNames}>Get Table Names</Button>
+
+      <Button onClick={getDatabaseSchema}>Get Database Schema</Button>
+
+      <input type="file" onChange={handleFileChange} />
+    </div>
   );
 }
 
---
+++ src/lib/sqlite.ts
@@ -0,0 +1,133 @@
+import initSqlJs from "sql.js";
+import type { Database, SqlJsStatic } from "sql.js";
+
+export default class Sqlite {
+  static debug = 0;
+  static sqlJsStatic?: SqlJsStatic;
+
+  private db: Database;
+
+  public tables: string[] = [];
+  public schema: Map<
+    string,
+    {
+      name: string;
+      cid: number;
+      type: string;
+      notnull: number;
+      dflt_value: string;
+      pk: number;
+    }[]
+  > = new Map();
+
+  private constructor(db: Database, isFile = false) {
+    this.db = db;
+
+    // Check if user is opening a file or creating a new database.
+    if (isFile) {
+      this.getTableNames();
+      this.getDatabaseSchema();
+    } else {
+      this.exec(
+        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)"
+      );
+      this.exec(
+        "CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, content TEXT)"
+      );
+      this.exec("INSERT INTO users (name) VALUES ('John')");
+      this.exec("INSERT INTO users (name) VALUES ('Jane')");
+      this.exec(
+        "INSERT INTO posts (user_id, title, content) VALUES (1, 'Hello', 'World')"
+      );
+      this.exec(
+        "INSERT INTO posts (user_id, title, content) VALUES (2, 'Hello', 'World')"
+      );
+      this.getDatabaseSchema();
+    }
+  }
+
+  private static async initSQLjs(): Promise<SqlJsStatic> {
+    if (Sqlite.sqlJsStatic) return Sqlite.sqlJsStatic;
+
+    Sqlite.debug += 1;
+    console.log("Initializing SQLite", Sqlite.debug);
+
+    return await initSqlJs({
+      locateFile: (file) => `https://sql.js.org/dist/${file}`,
+    });
+  }
+
+  public static async create(): Promise<Sqlite> {
+    const SQL = await Sqlite.initSQLjs();
+    const db = new SQL.Database();
+    return new Sqlite(db, false);
+  }
+
+  public static async open(file: Uint8Array): Promise<Sqlite> {
+    const SQL = await Sqlite.initSQLjs();
+    const db = new SQL.Database(file);
+    return new Sqlite(db, true);
+  }
+
+  public exec(sql: string) {
+    const results = this.db.exec(sql);
+    const upperSql = sql.toUpperCase();
+
+    // Update tables if the SQL statement is a CREATE TABLE statement.
+    if (upperSql.includes("CREATE TABLE")) {
+      this.getTableNames();
+      this.getDatabaseSchema(); // Update schema after creating a new table.
+    }
+
+    // Update schema if the SQL statement is ALTER TABLE || UPDATE statements.
+    if (upperSql.includes("ALTER TABLE") || upperSql.includes("UPDATE")) {
+      this.getDatabaseSchema();
+    }
+
+    return results;
+  }
+
+  private getTableNames() {
+    const results = this.exec(
+      "SELECT name FROM sqlite_master WHERE type='table'"
+    );
+
+    if (results.length === 0) return [];
+    this.tables = results[0].values.map((value) => value[0] as string);
+  }
+
+  private getTableSchema(tableName: string) {
+    const results = this.exec(`PRAGMA table_info(${tableName})`);
+
+    if (results.length === 0) throw new Error("Table not found");
+
+    const tableSchema: {
+      name: string;
+      cid: number;
+      type: string;
+      notnull: number;
+      dflt_value: string;
+      pk: number;
+    }[] = [];
+    for (const row of results[0].values) {
+      const [cid, name, type, notnull, dflt_value, pk] = row;
+      tableSchema.push({
+        name: name as string,
+        cid: cid as number,
+        type: type as string,
+        notnull: notnull as number,
+        dflt_value: dflt_value as string,
+        pk: pk as number,
+      });
+    }
+
+    this.schema.set(tableName, tableSchema);
+  }
+
+  private getDatabaseSchema() {
+    const tables = this.tables;
+    for (const table of tables) {
+      this.getTableSchema(table);
+    }
+  }
+}
