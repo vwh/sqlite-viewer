aaba5e65e24dd00bf5b22203c1fc70a23b5caede
vwh <vwhe@proton.me>
2025-03-18 09:41:35 +0300

    feat: optimize vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,61 @@
 import path from "node:path";
 import { defineConfig } from "vite";
+
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import MillionLint from "@million/lint";
+import compression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), MillionLint.vite()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    MillionLint.vite(),
+    compression(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
